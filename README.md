c3317eee37d404072de9936daa5d4443ac45ebbc
Manoj kumar <tech.manoj.madireddy@gmail.com>
2024-04-15 20:07:15 -0500

    sample -- Updated readme file and created a class to add elements to an array

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 Added readme file First Time.
+changed readme file second time.
---
+++ src/Strings/AddElementToArray.java
@@ -0,0 +1,18 @@
+package Strings;
+
+import java.util.Arrays;
+
+public class AddElementToArray {
+
+    public static void main(String[] args) {
+        int arr[] = new int[10];
+        arr[0] = 1;
+        arr[1] = 2;
+        arr[2] = 3;
+        arr[3] = 4;
+        arr[4] = 5;
+        arr[5] = 6;
+
+        System.out.println(Arrays.toString(arr));
+    }
+}
